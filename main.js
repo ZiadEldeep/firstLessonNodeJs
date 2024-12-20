5d04d382cb216b71c5c57945c0527d5518ece30b
Ziad Eldeep <ziaeldeep84@gmail.com>
2024-12-20 02:34:39 +0200

    12/9/2024 9:10pm

--- main.js
+++ main.js
@@ -1,29 +1,75 @@
-const http = require('http');  // Corrected the import statement
-const port=3000
+// // const http = require('http');  // Corrected the import statement
+// // const port=3000
 
-const app = http.createServer((req, res) => {
-    let myObj=[
-        {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
-        {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
-        {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
-        {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
-        {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
-        {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
-        {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
-        {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
-    ]
-    const {url,method} =req
-    if(url=='/'&&method=='GET'){
-        res.write('hello in home api')
-    }
-    else if(url=='/posts'){
-        res.write('hello posts api')
-    }
-    else{
-        res.write('<h1 style="color:red">404 NOT FOUND Page</h1>')        
-    }
-    res.end()
-});
-app.listen(3000, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// // const app = http.createServer((req, res) => {
+// //     let myObj=[
+// //         {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+// //         {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+// //         {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+// //         {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+// //         {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+// //         {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+// //         {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+// //         {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+// //     ]
+// //     const {url,method} =req
+// //     if(url=='/'&&method=='GET'){
+// //         res.write(JSON.stringify(myObj))
+// //     }
+// //     else if(url=='/posts'){
+// //         res.write('hello posts api')
+// //     }
+// //     else{
+// //         res.write('<h1 style="color:red">404 NOT FOUND Page</h1>')        
+// //     }
+// //     res.end()
+// // });
+// // app.listen(3000, () => {
+// //     console.log(`Server is running on port ${port}`);
+// // });
+// let myObj=[
+//             {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+//             {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+//             {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+//             {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+//             {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+//             {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+//             {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+//             {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+//         ]
+// const express=require('express')
+// const app=express()
+// app.get('/home',(req,res)=>{
+//     res.json(myObj)
+// })
+// app.listen(3000,()=>{
+//     console.log('server is running...');
+    
+// })
+let myObj=[
+            {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+            {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+            {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+            {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+            {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+            {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+            {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+            {name:'ziad',age:23,email:'ziaeldeep84@gmail.com'},
+        ]
+        const express=require('express')
+        const app=express()
+        app.use(express.json())
+        app.get('/home',(req,res)=>{
+            res.json(myObj)
+        })
+        app.post('/addUser',(req,res)=>{
+            console.log(req.body);          
+            res.send('addeeddddd')
+        })
+        app.get('*',(req,res)=>{
+            res.send('404 NOT FOUND PAGE')
+        })
+        app.listen(3000,()=>{
+            console.log('hiii');
+            
+        })
